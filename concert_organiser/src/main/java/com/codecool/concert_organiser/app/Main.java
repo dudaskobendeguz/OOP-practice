028b938cf5f4abd0d12b18e7471e5e6301e83972
dudaskobende <dudaskobende@gmail.com>
2022-06-08 15:08:08 +0200

    Add Concert Organiser task

--- concert_organiser/src/main/java/com/codecool/concert_organiser/app/Main.java
+++ concert_organiser/src/main/java/com/codecool/concert_organiser/app/Main.java
@@ -1,5 +1,8 @@
 package com.codecool.concert_organiser.app;
 
+
+//start 2022.06.08 15:06
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
